66972214068dd8ff01352673e2341e978625eaf0
ag <ahug79@gmail.com>
2022-09-29 19:37:55 +0100

    more main init try

--- mig_main.f90
+++ mig_main.f90
@@ -198,28 +198,28 @@ nonlabinc=0.0_dp !ahu030622
     !changing only type parameters in pars
     !pars(70)=0 !ptype set to 0 in getpars
     !pars(71)=0 !ptype set to 0 in getpars
-    pars(72)=8.8_dp !alf1t
+    pars(72)=9.6_dp !alf1t
     pars(73)=8.9_dp !alf2t
     !pars(74)=0 !cst1 this is set in getpars as it is a normalization
     pars(75)=0.1_dp !mumar1 !should this be set to 0? 
 
-    pars(76)=1.0_dp !ptypehs2 
-    pars(77)=1.0_dp !ptypecol2
-    pars(78)=9.6_dp !alf1t
+    pars(76)=0.1_dp !ptypehs2 
+    pars(77)=0.1_dp !ptypecol2
+    pars(78)=9.2_dp !alf1t
     pars(79)=8.9_dp !alf2t
     pars(80)=-100.0_dp !cst2 
     pars(81)=0.1_dp !mumar2    
     
-    pars(82)=1.0_dp !ptypehs3 
-    pars(83)=1.0_dp !ptypecol3
-    pars(84)=9.0_dp !alf1t
+    pars(82)=0.1_dp !ptypehs3 
+    pars(83)=0.1_dp !ptypecol3
+    pars(84)=9.1_dp !alf1t
     pars(85)=9.1_dp !alf2t
     pars(86)=-500.0_dp !cst3
     pars(87)=0.1_dp !mumar3
     
-    pars(88)=1.0_dp !ptypehs4 
-    pars(89)=1.0_dp !ptypecol4
-    pars(90)=9.8_dp !alf1t
+    pars(88)=0.1_dp !ptypehs4 
+    pars(89)=0.1_dp !ptypecol4
+    pars(90)=9.3_dp !alf1t
     pars(91)=9.2_dp !alf2t
     pars(92)=8000.0_dp !cst4 
     pars(93)=0.1_dp !mumar4
@@ -233,28 +233,28 @@ nonlabinc=0.0_dp !ahu030622
     !changing only type parameters in pars
     !pars(70)=0 !ptype set to 0 in getpars
     !pars(71)=0 !ptype set to 0 in getpars
-    pars(72)=8.6_dp !alf1t
+    pars(72)=9.2_dp !alf1t
     pars(73)=8.9_dp !alf2t
     !pars(74)=0 !cst1 this is set in getpars as it is a normalization
     pars(75)=0.1_dp !mumar1 !should this be set to 0? 
 
-    pars(76)=1.0_dp !ptypehs2 
-    pars(77)=1.0_dp !ptypecol2
+    pars(76)=0.1_dp !ptypehs2 
+    pars(77)=0.1_dp !ptypecol2
     pars(78)=9.6_dp !alf1t
     pars(79)=8.9_dp !alf2t
     pars(80)=-100.0_dp !cst2 
     pars(81)=0.1_dp !mumar2    
     
-    pars(82)=1.0_dp !ptypehs3 
-    pars(83)=1.0_dp !ptypecol3
-    pars(84)=9.0_dp !alf1t
+    pars(82)=0.1_dp !ptypehs3 
+    pars(83)=0.1_dp !ptypecol3
+    pars(84)=9.1_dp !alf1t
     pars(85)=9.1_dp !alf2t
     pars(86)=-500.0_dp !cst3
     pars(87)=0.1_dp !mumar3
     
-    pars(88)=1.0_dp !ptypehs4 
-    pars(89)=1.0_dp !ptypecol4
-    pars(90)=9.9_dp !alf1t
+    pars(88)=0.1_dp !ptypehs4 
+    pars(89)=0.1_dp !ptypecol4
+    pars(90)=9.3_dp !alf1t
     pars(91)=9.2_dp !alf2t
     pars(92)=8000.0_dp !cst4 
     pars(93)=0.1_dp !mumar4
@@ -274,32 +274,32 @@ nonlabinc=0.0_dp !ahu030622
     !changing only type parameters in pars
     !pars(70)=0 !ptype set to 0 in getpars
     !pars(71)=0 !ptype set to 0 in getpars
-    pars(72)=8.8_dp !alf1t
+    pars(72)=9.2_dp !alf1t
     pars(73)=8.9_dp !alf2t
     !pars(74)=0 !cst1 this is set in getpars as it is a normalization
     pars(75)=0.1_dp !mumar1 !should this be set to 0? 
 
-    pars(76)=1.0_dp !ptypehs2 
-    pars(77)=1.0_dp !ptypecol2
-    pars(78)=9.6_dp !alf1t
+    pars(76)=0.1_dp !ptypehs2 
+    pars(77)=0.1_dp !ptypecol2
+    pars(78)=9.1_dp !alf1t
     pars(79)=8.9_dp !alf2t
     pars(80)=-100.0_dp !cst2 
     pars(81)=0.1_dp !mumar2    
     
-    pars(82)=1.0_dp !ptypehs3 
-    pars(83)=1.0_dp !ptypecol3
-    pars(84)=9.0_dp !alf1t
+    pars(82)=0.1_dp !ptypehs3 
+    pars(83)=0.1_dp !ptypecol3
+    pars(84)=9.6_dp !alf1t
     pars(85)=9.1_dp !alf2t
     pars(86)=-500.0_dp !cst3
     pars(87)=0.1_dp !mumar3
     
-    pars(88)=1.0_dp !ptypehs4 
-    pars(89)=1.0_dp !ptypecol4
-    pars(90)=9.8_dp !alf1t
+    pars(88)=0.1_dp !ptypehs4 
+    pars(89)=0.1_dp !ptypecol4
+    pars(90)=9.3_dp !alf1t
     pars(91)=9.2_dp !alf2t
     pars(92)=8000.0_dp !cst4 
-    pars(93)=0.1_dp !mumar4
-    pars(42:50)=pars1(42:50)-0.3
+    pars(93)=0.1_dp !mumar4    
+    !pars(42:50)=pars1(42:50)-0.3
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp     
 
@@ -311,33 +311,33 @@ nonlabinc=0.0_dp !ahu030622
     !changing only type parameters in pars
     !pars(70)=0 !ptype set to 0 in getpars
     !pars(71)=0 !ptype set to 0 in getpars
-    pars(72)=8.8_dp !alf1t
+    pars(72)=9.2_dp !alf1t
     pars(73)=8.9_dp !alf2t
     !pars(74)=0 !cst1 this is set in getpars as it is a normalization
     pars(75)=0.1_dp !mumar1 !should this be set to 0? 
 
-    pars(76)=1.0_dp !ptypehs2 
-    pars(77)=1.0_dp !ptypecol2
-    pars(78)=9.6_dp !alf1t
+    pars(76)=0.1_dp !ptypehs2 
+    pars(77)=0.1_dp !ptypecol2
+    pars(78)=9.1_dp !alf1t
     pars(79)=8.9_dp !alf2t
     pars(80)=-100.0_dp !cst2 
     pars(81)=0.1_dp !mumar2    
     
-    pars(82)=1.0_dp !ptypehs3 
-    pars(83)=1.0_dp !ptypecol3
-    pars(84)=9.0_dp !alf1t
+    pars(82)=0.1_dp !ptypehs3 
+    pars(83)=0.1_dp !ptypecol3
+    pars(84)=9.3_dp !alf1t
     pars(85)=9.1_dp !alf2t
     pars(86)=-500.0_dp !cst3
     pars(87)=0.1_dp !mumar3
     
-    pars(88)=1.0_dp !ptypehs4 
-    pars(89)=1.0_dp !ptypecol4
-    pars(90)=9.8_dp !alf1t
+    pars(88)=0.1_dp !ptypehs4 
+    pars(89)=0.1_dp !ptypecol4
+    pars(90)=9.6_dp !alf1t
     pars(91)=9.2_dp !alf2t
     pars(92)=8000.0_dp !cst4 
     pars(93)=0.1_dp !mumar4
-    pars(42:50)=pars1(42:50)-0.3
-    pars(44)=0.0_dp
+    !pars(42:50)=pars1(42:50)-0.3
+    !pars(44)=0.0_dp
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp    
 
