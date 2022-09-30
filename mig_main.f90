64f52d29be0fd763d83e0121a8fc23e8c93df93b
ag <ahug79@gmail.com>
2022-09-30 17:52:23 +0100

    ignore: all same as prev but pmeet lower to dec getmar
    ignore because I had an extra call to bjval so the moments file did not wirte property. look at the next run and tag m093022_magic5 instead.

--- mig_main.f90
+++ mig_main.f90
@@ -254,7 +254,24 @@ nonlabinc=0.0_dp !ahu030622
     pars(92)=-5000.0_dp 
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
+
+    open(unit=2,file='bp093022.txt',status='old',action='read') ; read(2,*) pars1	; close(2)
+    nonlabinc=0.0_dp
+    pars=pars1
+    pars(75)=0.0015_dp !mumar1
+    pars(81)=0.0015_dp !mumar2
+    pars(87)=0.0015_dp !mumar3
+    pars(93)=0.1_dp    !mumar4
+    pars(68:69)=30000.0_dp   
+    !pars(74)= cst1 norm to 0 in getpars
+    pars(80)=-30000.0_dp
+    pars(86)=-30000.0_dp
+    pars(92)=-5000.0_dp 
+    pars(21)=-2.5_dp !pmeet to bring down getmar rates
+    call getpars(pars,realpars)
+    call objfunc(pars,qval) ; realpars=realpartemp   
  
+
     !mytime(iam+1,1)=secnds(0.0)
     !mytime(iam+1,2)=secnds(mytime(iam+1,1))        
     !if (iam==0) print*, 'Here is qval: ', qval
