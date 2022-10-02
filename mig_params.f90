52cf5ad163fd2e3ea3399079d180a235e9dc3bfc
ag <ahug79@gmail.com>
2022-10-02 23:16:00 +0100

    NEXP=2! to check how much it decreases runtime

--- mig_params.f90
+++ mig_params.f90
@@ -43,7 +43,7 @@
 	!character(len=15), dimension(npars) :: parname 
 	integer(i4b), parameter :: mna=18,mxa=50   !,mxai=50		!ahu 070713 40	!if you ever change mina from 16 that might be problematic, because of psiddata%home and simdata%home definitions. look in read_data and read simdata for this
     integer(i4b), parameter :: MNAD=MNA-1,MXAD=MXA-1            !ahu jan19 010219
-    integer(i4b), parameter :: nh=2,nexp=4,nsimeach=10,neduc=2,nkid=2 !kid is 1 if no kid,2 if yes kid !ahu 0327 changed nsimeach from 10 to 5
+    integer(i4b), parameter :: nh=2,nexp=2,nsimeach=10,neduc=2,nkid=2 !kid is 1 if no kid,2 if yes kid !ahu 0327 changed nsimeach from 10 to 5
 	integer(i4b), parameter :: np=3,nz=1 !ag090122 agsept2022 changed nz frmo 1 to 5 !ahu 121818 changed from 3 to 6 !ahu 0327 changed np from 5 to 2
 	integer(i4b), parameter :: nqs = (np+2) *  nl
 	integer(i4b), parameter :: nq  = (np+2) * (np+2) * nl
