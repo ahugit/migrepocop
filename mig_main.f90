09f662af2c6534d23bf5724983fb4974839050d8
ag <ahug79@gmail.com>
2022-10-03 22:23:44 +0100

    pars(9)=0.5_dp !u cur m
    (to bring up eu move and ee move so that they don't say I'll find a job after I movey)

--- mig_main.f90
+++ mig_main.f90
@@ -226,48 +226,22 @@ nonlabinc=0.0_dp !ahu030622
     open(unit=2,file='bp093022.txt',status='old',action='read') ; read(2,*) pars1	; close(2)
     nonlabinc=0.0_dp
     pars=pars1
-    pars(75)=0.0015_dp !mumar1
-    pars(81)=0.0015_dp !mumar2
-    pars(87)=0.0015_dp !mumar3
-    pars(93)=0.1_dp    !mumar4
+    pars(75)=0.0005_dp !mumar1
+    pars(81)=0.0005_dp !mumar2
+    pars(87)=0.0005_dp !mumar3
+    pars(93)=0.01_dp    !mumar4
     pars(68:69)=30000.0_dp   
     !pars(74)= cst1 norm to 0 in getpars
-    pars(80)=-15000.0_dp
-    pars(86)=-15000.0_dp
-    pars(92)=-1000.0_dp 
-    !call getpars(pars,realpars)
-    !call objfunc(pars,qval) ; realpars=realpartemp   
- 
-    open(unit=2,file='bp093022.txt',status='old',action='read') ; read(2,*) pars1	; close(2)
-    nonlabinc=0.0_dp
-    pars=pars1
-    pars(75)=0.0015_dp !mumar1
-    pars(81)=0.0015_dp !mumar2
-    pars(87)=0.0015_dp !mumar3
-    pars(93)=0.1_dp    !mumar4
-    pars(68:69)=30000.0_dp   
-    !pars(74)= cst1 norm to 0 in getpars
-    pars(80)=-30000.0_dp
-    pars(86)=-30000.0_dp
-    pars(92)=-5000.0_dp 
-    !call getpars(pars,realpars)
-    !call objfunc(pars,qval) ; realpars=realpartemp   
-
-    open(unit=2,file='bp093022.txt',status='old',action='read') ; read(2,*) pars1	; close(2)
-    nonlabinc=0.0_dp
-    pars=pars1
-    pars(75)=0.0015_dp !mumar1
-    pars(81)=0.0015_dp !mumar2
-    pars(87)=0.0015_dp !mumar3
-    pars(93)=0.1_dp    !mumar4
-    pars(68:69)=30000.0_dp   
-    !pars(74)= cst1 norm to 0 in getpars
-    pars(80)=-30000.0_dp
-    pars(86)=-30000.0_dp
-    pars(92)=-5000.0_dp 
+    pars(80)=-80000.0_dp
+    pars(86)=-80000.0_dp
+    pars(92)=-25000.0_dp 
     pars(21)=-2.5_dp !pmeet to bring down getmar rates
-    !call getpars(pars,realpars)
-    !call objfunc(pars,qval) ; realpars=realpartemp   
+    pars(10)=1.5_dp !u of m to bring up e u cond on move transitions (it's currentlt -1.2 which is 0.2 sth)
+    pars(12)=0.1_dp !u of fem to bring up e u cond on move transitions (it's currently -2.7 which is 0.06)
+    pars(52)=-1.8_dp !increasing alf12 because of nexp
+    pars(64)=-2.2_dp !increasing alf22 because of nexp
+    call getpars(pars,realpars)
+    call objfunc(pars,qval) ; realpars=realpartemp   
  
 
     open(unit=2,file='bp093022.txt',status='old',action='read') ; read(2,*) pars1	; close(2)
@@ -287,6 +261,7 @@ nonlabinc=0.0_dp !ahu030622
     pars(12)=0.1_dp !u of fem to bring up e u cond on move transitions (it's currently -2.7 which is 0.06)
     pars(52)=-1.8_dp !increasing alf12 because of nexp
     pars(64)=-2.2_dp !increasing alf22 because of nexp
+    pars(9)=0.5_dp !u cur m (to bring up eu move and ee move so that they don't say I'll find a job after I movey)
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
 
