53a50d0e23923a053f92b0db63279904af6c6faa
ag <ahug79@gmail.com>
2022-10-08 20:07:48 +0100

    prev run was w/ skriv=false. now try skriv time
    this is the first time skriv true after the big yaz changes so we will see if it works
    numit=1

--- mig_main.f90
+++ mig_main.f90
@@ -132,7 +132,7 @@ program main
 		open(unit=64, file='chkstep.txt',status='replace')
 	end if     
 	!ahu030622 skriv=.false. ; if (iam==24.and.iter==1) skriv=.false.  !ahu 0327 iam=9
-	skriv=.false. ; if (iam==0.and.iter==1) skriv=.false.  !ahu030622
+	skriv=.false. ; if (iam==0.and.iter==1) skriv=.true.  !ahu030622
     if (.not.groups) skriv=.false. !ahu 0327
     iwritemom=0 ; if (iam==0) iwritemom=1 
 	if (skriv) then 	
@@ -265,13 +265,13 @@ nonlabinc=0.0_dp !ahu030622
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
 
-    pars(16)=pars1(16)
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
+    !pars(16)=pars1(16)
+    !call getpars(pars,realpars)
+    !call objfunc(pars,qval) ; realpars=realpartemp   
 
-    pars(16)=20.0_dp
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
+    !pars(16)=20.0_dp
+    !call getpars(pars,realpars)
+    !call objfunc(pars,qval) ; realpars=realpartemp   
 
 
 !*************************    
