770c6badf335abcf223599675757dd984232b124
ag <ahug79@gmail.com>
2022-10-08 23:49:51 +0100

    wdif ident: exp up zero and sigwge zero (almost)
    typemoments=false, so the objval is not comparable to previous ones here
    also numit=2

--- mig_main.f90
+++ mig_main.f90
@@ -132,7 +132,7 @@ program main
 		open(unit=64, file='chkstep.txt',status='replace')
 	end if     
 	!ahu030622 skriv=.false. ; if (iam==24.and.iter==1) skriv=.false.  !ahu 0327 iam=9
-	skriv=.false. ; if (iam==0.and.iter==1) skriv=.true.  !ahu030622
+	skriv=.false. ; if (iam==0.and.iter==1) skriv=.false.  !ahu030622
     if (.not.groups) skriv=.false. !ahu 0327
     iwritemom=0 ; if (iam==0) iwritemom=1 
 	if (skriv) then 	
@@ -262,6 +262,10 @@ nonlabinc=0.0_dp !ahu030622
     pars(92)=-50000.0_dp 
     pars(93)=0.01_dp    !mumar4
     pars(16)=-20.0_dp
+    call getpars(pars,realpars)
+    call objfunc(pars,qval) ; realpars=realpartemp   
+
+    pars(66)=pars1(16)-3.0_dp
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
 
