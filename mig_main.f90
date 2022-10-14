b1efcdcffd9f64d4a5d09746bf1008d53468076b
ag <ahug79@gmail.com>
2022-10-14 15:45:33 +0100

    setting pars in main to try huge major change

--- mig_main.f90
+++ mig_main.f90
@@ -277,7 +277,7 @@ nonlabinc=0.0_dp !ahu030622
     pars(67)=-2.0_dp    !sigwgem
     pars(68)=3.0_dp     !sigom
     pars(69)=-2.0_dp    !sigof
-    pars(1)=1.0_dp
+    !pars(1)=1.0_dp
     !pars(22)=3000.0_dp !uhome1
     !pars(13)=4.0_dp    !psil
     call getpars(pars,realpars)
