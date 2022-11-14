996b04cfcbeb4537af8682fe92d61ce3e6973431
ag <ahug79@gmail.com>
2022-11-14 00:53:01 +0000

    CSTS NOT EQ BY TYP. wdif move positive for once so banging on that one

--- mig_main.f90
+++ mig_main.f90
@@ -336,8 +336,8 @@ nonlabinc=0.0_dp !ahu030622
     pars1(25)=pars1(25)+50000.0_dp
     pars1(26)=pars1(26)+1.5_dp
     pars=pars1
-    pars(68)=-2.5_dp
-    pars(69)=-2.5_dp
+    pars(68)=-2.0_dp
+    pars(69)=-2.0_dp
     pars(14)=0.0_dp !ucst
     pars(15)=-10.0_dp !agecst
     pars(24)=0.0_dp !ecst
@@ -353,19 +353,26 @@ nonlabinc=0.0_dp !ahu030622
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
 
-    pars(68)=-2.0_dp
-    pars(69)=-2.0_dp
+    pars(74)=-10000.0_dp
+    pars(80)=-10000.0_dp
+    pars(86)=-10000.0_dp
+    pars(92)=-10000.0_dp
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
 
-    pars(74)=-40000.0_dp
+    pars(74)=-20000.0_dp
+    pars(80)=-20000.0_dp
+    pars(86)=-20000.0_dp
+    pars(92)=-20000.0_dp
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
 
-    pars(74)=-60000.0_dp
+    pars(74)=-30000.0_dp
+    pars(80)=-30000.0_dp
+    pars(86)=-30000.0_dp
+    pars(92)=-30000.0_dp
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
-    !pars(26) divpen
 
 !if (iwritegen==1) then
 !    print*, "divpen", pars(26),logit(pars(26)),multdiv*logit(pars(26))
