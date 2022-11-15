56596de10602b9768dd95bca204fac49d9892b18
ag <ahug79@gmail.com>
2022-11-15 02:53:52 +0000

    sigo and higher uhome

--- mig_main.f90
+++ mig_main.f90
@@ -397,33 +397,44 @@ nonlabinc=0.0_dp !ahu030622
  
     pars(15)=-40.0_dp !agecst
 
-    pars(74)=-10000.0_dp
-    pars(80)=-5000.0_dp
-    pars(86)=-15000.0_dp
-    pars(92)=-20000.0_dp
-    
+    !pars(74)=-10000.0_dp
+    !pars(80)=-5000.0_dp
+    !pars(86)=-15000.0_dp
+    !pars(92)=-20000.0_dp
+
     pars(68:69)=-4.0_dp 
-    pars(22:23)=5000.0_dp
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
-    pars(22:23)=10000.0_dp
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
     pars(22:23)=15000.0_dp
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
+    pars(22:23)=30000.0_dp
+    call getpars(pars,realpars)
+    call objfunc(pars,qval) ; realpars=realpartemp   
+    pars(22:23)=45000.0_dp
+    call getpars(pars,realpars)
+    call objfunc(pars,qval) ; realpars=realpartemp   
+
+    pars(68:69)=-3.0_dp 
+    pars(22:23)=15000.0_dp
+    call getpars(pars,realpars)
+    call objfunc(pars,qval) ; realpars=realpartemp   
+    pars(22:23)=30000.0_dp
+    call getpars(pars,realpars)
+    call objfunc(pars,qval) ; realpars=realpartemp   
+    pars(22:23)=45000.0_dp
+    call getpars(pars,realpars)
+    call objfunc(pars,qval) ; realpars=realpartemp   
+
 
     pars(68:69)=-2.0_dp 
-    pars(22:23)=5000.0_dp
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
-    pars(22:23)=10000.0_dp
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
     pars(22:23)=15000.0_dp
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
-
+    pars(22:23)=30000.0_dp
+    call getpars(pars,realpars)
+    call objfunc(pars,qval) ; realpars=realpartemp   
+    pars(22:23)=45000.0_dp
+    call getpars(pars,realpars)
+    call objfunc(pars,qval) ; realpars=realpartemp   
 
 
    
