45a4ac622567248af86cfcc60433007a0ee3d25c
ag <ahug79@gmail.com>
2022-11-16 04:18:29 +0000

    sending another opt

--- mig_objf.f90
+++ mig_objf.f90
@@ -341,7 +341,7 @@ contains
 	!totcostsave(1:nepsmove,iter)=moveshock_m(1:nepsmove)+cst(4)
 	!totcostname='total cost'
 	do i=1,npars
-        write(60,'(1a15,2f9.1)') parname(i),realpar_save(i,1:2) 
+        write(60,'(1a15,2f9.1)') parname(i),realpar_save(i,1:numit) 
 	end do 
 !	do i=1,npars
 !        write(60,'(1a15,9f9.1)') parname(i),realpar_save(i,3:11) !
