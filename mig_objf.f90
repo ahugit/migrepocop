6de37e03fce1317e30bf90ffeb048a85d65c23b4
ag <ahug79@gmail.com>
2022-11-16 11:31:57 +0000

    policytax with higher pmeet

--- mig_objf.f90
+++ mig_objf.f90
@@ -335,7 +335,11 @@ contains
     do i=1,nmom
 		if (momwhich(i)>10.and.momwhich(i)<50) then
         	write(6091115,'(i5,4i4,8F14.2)')	i,momwhich(i),momage(i),momsex(i),momrel(i),momsim_save(i,1:numit)  !,momdat_save(i,1)
-		else if (momwhich(i)>50) then
+		end if 
+	end do
+
+    do i=1,nmom
+		if (momwhich(i)>50) then
 			write(6091115,'(i5,4i4,8F14.2)')	i,momwhich(i),momage(i),momsex(i),momrel(i),momsim_save(i,1:numit)  !,momdat_save(i,1)	
 		end if 
 	end do
