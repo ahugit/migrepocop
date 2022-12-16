cf2b00864a462879191070bf497a4af702d9dc54
ag <ahug79@gmail.com>
2022-12-16 20:14:30 +0000

    same opt but with lower weights on wvar and wdif
    so that rel and wage weights in general have higher weights now compared to the previous opt. the previous opt did not work out I think so I did stop it at some point. wvar weights way too high so it messes up wages, mar rates etc.

--- mig_params.f90
+++ mig_params.f90
@@ -84,8 +84,8 @@
 	real(dp), parameter :: maxw=150.0_dp                ! upper truncation point of male log wage
 	real(dp), parameter :: pen=-99999999.0_dp
 	integer(i4b), parameter :: ipen=-99999	
-    real(dp), parameter :: wwage=50.0_dp,wwvar=10000.0_dp,wdifww=50.0_dp,whour=50.0_dp
-    real(dp), parameter :: wrel=1.0_dp,wmove=1.0_dp,whome=10.0_dp,wkid=1.0_dp,wprop=1.0_dp		!ahu 121918 changed wmove to 10 from 1 and changed wmovemar from 10 to 100		! weights for moments for married couples. set in objfunc.
+    real(dp), parameter :: wwage=50.0_dp,wwvar=50.0_dp,wdifww=50.0_dp,whour=50.0_dp
+    real(dp), parameter :: wrel=100.0_dp,wmove=1.0_dp,whome=10.0_dp,wkid=1.0_dp,wprop=1.0_dp		!ahu 121918 changed wmove to 10 from 1 and changed wmovemar from 10 to 100		! weights for moments for married couples. set in objfunc.
     !    real(dp), parameter :: wtrans=100.0_dp,wwaged=10.0_dp,wdifww=100.0_dp,wrel=1.0_dp,wmove=10.0_dp,whour=1.0_dp,wwvar=100.0_dp
 !    real(dp), parameter :: wwage=1.0_dp,wkid=1.0_dp,wmovemar=1.0_dp,wmovesin=1.0_dp,wwagebymove=1.0_dp		!ahu 121918 changed wmove to 10 from 1 and changed wmovemar from 10 to 100		! weights for moments for married couples. set in objfunc.
     character(len=23) :: datafilename  != 'familymigpsid.txt' ! data filename set in main now
