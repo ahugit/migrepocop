01f3bfe8d6dea9e8ad7c10af139e34203a5e6881
ag <ahug79@gmail.com>
2022-12-16 17:52:03 +0000

    more runs with divpen/mumar
    trying to see what will get me enough marriage rates but without too high divmove's. so increased divpen and also increased mumar. But note that when mumar increases too much then all types marry at the same rate and that's not really very good. so beware of that.

--- mig_main.f90
+++ mig_main.f90
@@ -351,23 +351,11 @@ program main
     pars(90)=9.7_dp
     pars(91)=9.0_dp
     pars(26)=pars(26)-3.0_dp
+    pars(75)=pars(75)+0.5_dp
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
 
-    pars(26)=pars(26)+1.0_dp
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
-
-    pars(26)=pars(26)+1.0_dp
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
-
-    pars(26)=pars(26)-2.0_dp
-    pars(75)=pars(75)+1.0_dp
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
-
-    pars(75)=pars(75)+1.0_dp
+    pars(75)=pars(75)+0.5_dp
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
 
