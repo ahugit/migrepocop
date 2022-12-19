44fda06007e78e2038e91ddd1e69d5a418259a43
ag <ahug79@gmail.com>
2022-12-19 18:55:54 +0000

    TERMINALVAL=.FALSE. now try poltax comp (poltax=0/then =6/then taxset=0)
    Indeed this was it. In other words, now policytax=6 and taxset=0 give the same output. AT LAST.  So terminalval was indeed the issue. 
    Note that in the poltaxcompare branch, you did the comparisons a little wrong. you first ran taxset=0 and then policytax=6, but then policytax value won't make a difference anyway since taxset is already 0 at that point. It doesn't matter anyway since the problem turned out to be terminalval, but NOTE THIS WHEN YOU ARE LOOKING AT THE OUTPUT OF THE POLTAXCOMPARE BRANCH.

--- mig_main.f90
+++ mig_main.f90
@@ -356,33 +356,15 @@ program main
     open(unit=2,file='o121822_1bpobj.txt',status='old',action='read') ; read(2,*) pars	; close(2)
     pars(12)=pars(12)+1.5_dp
 
-    open(unit=2,file='bpobjsofar.txt',status='old',action='read') ; read(2,*) pars	; close(2)
-    pars(72)=9.21_dp
-    pars(73)=8.67_dp
-    pars(78)=9.29_dp
-    pars(79)=8.86_dp
-    pars(84)=8.85_dp
-    pars(85)=9.08_dp
-    pars(90)=9.03_dp
-    pars(91)=8.77_dp
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
-    policytax=1
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
-    policytax=2
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
-    policytax=3
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
-    policytax=4
-    call getpars(pars,realpars)
-    call objfunc(pars,qval) ; realpars=realpartemp   
-    policytax=5
+    open(unit=2,file='o121922_1bpobj.txt',status='old',action='read') ; read(2,*) pars	; close(2)
+    terminalval=.FALSE.
+    pars(75)=-7.0_dp
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
     policytax=6
+    call getpars(pars,realpars)
+    call objfunc(pars,qval) ; realpars=realpartemp   
+    taxset=0
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp   
 
