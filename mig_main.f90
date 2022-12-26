18c9227b1d6bec1eb4c22ad125357e780690a0f5
ag <ahug79@gmail.com>
2022-12-26 18:38:20 +0000

    o121522_1 termval true sigwge higher,sigo more higher

--- mig_main.f90
+++ mig_main.f90
@@ -345,7 +345,7 @@ program main
     !call objfunc(pars,qval) ; realpars=realpartemp       
     terminalval=.true. ; ntermval=5 !doesn't matter what ntermval is when term is FALSE
     pars(66)=pars1(66)-0.5_dp
-    pars(68)=pars1(68)
+    pars(68)=pars1(68)+1.0_dp
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp       
     do i=1,4
@@ -353,6 +353,8 @@ program main
     call getpars(pars,realpars)
     call objfunc(pars,qval) ; realpars=realpartemp       
     end do
+
+
 
 	if (optimize) then 
 		! call simplex: the call to minim (the simplex routine) is set up to work with starting vector parvector
